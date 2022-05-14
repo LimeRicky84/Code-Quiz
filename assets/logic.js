4134580e8bfe03dafd4769367a113a4e5dae8594
Ricma <ric.madse@gmail.com>
2022-05-13 20:35:47 -0600

    pseudocode for js added

---
+++ assets/logic.js
@@ -0,0 +1,48 @@
+// List variables and constants
+
+
+
+
+
+
+// Function to start the quiz, timer, hide start screen, reveal question screen
+
+
+
+
+
+// function to get question from the list, clear old question choices
+
+// event listener for click choice
+
+
+
+// function for question click
+
+// if wrong, penalize time
+
+// if right, reward somehow and move on to next queston
+
+
+
+
+// function to end the quiz, stop timer, show end screen and final score
+
+
+
+
+
+
+// timer function.  End quiz if ran out of time
+
+
+
+
+
+
+// Save high score function
+
+
+
+
+
