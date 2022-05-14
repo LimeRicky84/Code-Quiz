82807c1e7aaa3a40d087bbdfd84cb5b090f5eaca
Ricma <ric.madse@gmail.com>
2022-05-14 15:39:11 -0600

    questions and answers added

--- assets/questions.js
+++ assets/questions.js
@@ -2,33 +2,33 @@
 
 var questions = [
     {
-        title: "Question number one"
-        choices: ['answers', 'answers']
-        answer: "answer"
+        title: "Which html tag contains javascript?"
+        choices: ['<p>', '<link>', '<js>', '<script>']
+        answer: '<script>'
     },
     {
-        title: "Question number one"
-        choices: ['answers', 'answers']
-        answer: "answer"
+        title: "How do you declare a variable in CSS?"
+        choices: ['-- (double hyphen)', '.var', '<variable>', ':: (double colon)']
+        answer: '-- (double hyphen)'
     },
     {
-        title: "Question number one"
-        choices: ['answers', 'answers']
-        answer: "answer"
+        title: "How do you return a reference from html in javascript?"
+        choices: ['<get-element>', 'https://...', 'document.getElementById()', '<a href="...">']
+        answer: 'document.getElementById()'
     },
     {
-        title: "Question number one"
-        choices: ['answers', 'answers']
-        answer: "answer"
+        title: "Where should your javascript link be located on an html file?"
+        choices: ['in the header', 'in the footer', 'at the end of the file', 'in an <aside>']
+        answer: 'at the end of the file'
     },
     {
-        title: "Question number one"
-        choices: ['answers', 'answers']
-        answer: "answer"
+        title: "console.log() will display data on a webpage."
+        choices: ['True', 'False']
+        answer: 'False'
     },
     {
-        title: "Question number one"
-        choices: ['answers', 'answers']
-        answer: "answer"
+        title: "How do you NOT decleare a variable in javascript?"
+        choices: ['var', 'let', '.style', 'const']
+        answer: '.style'
     },
 ]
